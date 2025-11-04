02576a5e78a0bcd487caae3e3b6454e42b158859
Brent Mercado <brentallen.mercado@benilde.edu.ph>
2025-11-05 07:45:29 +0800

    Added: init student class

---
+++ student_crud.py
@@ -0,0 +1,5 @@
+class Student(object):
+    def __init__(self, last_name, first_name, course):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.course = course
