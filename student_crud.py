7e68876e84651e53d33501cc2b8d58f38c851308
Brent Mercado <brentallen.mercado@benilde.edu.ph>
2025-11-05 07:59:10 +0800

    Menu: create student

--- student_crud.py
+++ student_crud.py
@@ -1,9 +1,10 @@
 class Student(object):
-    def __init__(self, last_name, first_name, course):
+    def __init__(self, last_name, first_name, course, s_id):
         self.last_name = last_name
         self.first_name = first_name
         self.course = course
 
+ListOfStudents = []
 while True:
     print("--- STUDENT CRUD PROJECT ---")
     print("1 - Add Student\n2 - View Students\n3 - Delete Students\n4 - Edit Students")
@@ -18,3 +19,16 @@ while True:
         except ValueError:
             print("Invalid Choice\n")
             continue
+
+    if choice == 1:
+        print("\n\n--- You are now creating a Student ---")
+        lname = input("Enter student last name: ")
+        fname = input("Enter student first name: ")
+        c = input("Enter student course: ")
+        student_id = input("Enter student id: ")
+        newStudent = Student(lname, fname, c, student_id)
+        ListOfStudents.append(newStudent)
+
+        print("\nYou have successfully created a Student!")
+        input("Press enter to continue...")
+        print("\n")
